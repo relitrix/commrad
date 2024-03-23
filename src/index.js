9775fd69f86e712f3db1d506d0ea3d66626c8412
Relitrix <mega.strilets2@gmail.com>
2024-03-23 15:38:25 +0300

    🚧 init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { TOKEN } = process.env
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(TOKEN);
