37ac2d0d934a34ed0ee40fc4c2db04d08eb1214f
Relitrix <mega.strilets2@gmail.com>
2024-03-29 15:43:41 +0300

    🎨 Рефакторинг commentsWave

--- src/jobs/commentsWave.js
+++ src/jobs/commentsWave.js
@@ -4,6 +4,30 @@ module.exports = async (client) => {
     const buildEmbed = require('../utils/buildEmbed')
     const chunkArray = require('../utils/chunkArray')
 
+    async function embeds(newComms) {
+        const promises = newComms.map(async comm => {
+            let basicInfo = null
+            try {
+                if (comm.vidId)
+                    basicInfo = await process.youtube.getBasicInfo(comm.vidId)
+            } catch (e) {
+                console.log("Failed to get basicInfo")
+                console.error(e)
+            }
+            return buildEmbed({
+                title: basicInfo?.basic_info?.title,
+                authorLink: comm.authorLink,
+                authorName: comm.authorName,
+                authorPic: comm.authorPic,
+                content: comm.content,
+                date: comm.date,
+                vidLink: basicInfo ? `https://youtube.com/watch?v=${comm.vidId}&lc=${comm.id}` : null,
+                vidThumbnail: basicInfo?.basic_info?.thumbnail[0].url
+            })
+        })
+        return await Promise.all(promises)
+    }
+
 
     const guildAndPair = await GuildSchema.find({ Pairs: { $exists: true, $not: { $size: 0 } } }, { Guild: 1, Pairs: 1 })
 
@@ -15,30 +39,11 @@ module.exports = async (client) => {
                 fetchComms(pair.youtubeChannel).then(result => {
                     if (!result.length) return
                     const newComms = result.filter(comm => comm.date > pair.date)
-                    const embeds = []
-                    const promises = []
-                    newComms.forEach(comm => {
-                        promises.push(process.youtube.getBasicInfo(comm.vidId).then(result => {
-                            console.log(result)
-                            embeds.push(buildEmbed({
-                                title: result?.basic_info?.title,
-                                authorLink: comm.authorLink,
-                                authorName: comm.authorName,
-                                authorPic: comm.authorPic,
-                                content: comm.content,
-                                date: comm.date,
-                                vidLink: `https://youtube.com/watch?v=${comm.vidId}&lc=${comm.id}`,
-                                vidThumbnail: result?.basic_info?.thumbnail[0].url
-                            }))
-                        }))
-                    })
-                    Promise.all(promises).then(() => {
-                        const chunks = chunkArray(embeds, 10)
-                        console.log(chunks.length)
+                    embeds(newComms).then((results) => {
+                        const chunks = chunkArray(results, 10)
                         chunks.forEach(async chunk => {
                             await discordChannel.send({ embeds: chunk })
                         })
-                        console.log(newComms.length)
                     })
                 }).then(async () => {
                     await GuildSchema.updateOne({ Guild: guild.id, Pairs: { $elemMatch: { discordChannel: pair.discordChannel, youtubeChannel: pair.youtubeChannel } } }, { "Pairs.$[].date": new Date() })
